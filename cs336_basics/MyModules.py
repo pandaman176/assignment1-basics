fix gradient explode reason: rms initialization with torch.empty

cs336_basics/MyModules.py
@@ -65,7 +65,7 @@ class RMSNorm(nn.Module):
         dtype: torch.dtype | None = None Data type of the parameters
         """
         super().__init__()
-        self.scale = nn.Parameter(torch.empty(d_model, device=device, dtype=dtype))
+        self.scale = nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
         self.eps = eps
         self.d_model = d_model
 
@@ -77,8 +77,9 @@ class RMSNorm(nn.Module):
         sq_mean = reduce(x**2, "batch sequence d_model -> batch sequence 1", "mean")
         rms = torch.sqrt(sq_mean + self.eps)
         norm_x = x / rms
+        result = norm_x * self.scale
 
-        return norm_x * self.scale
+        return result.to(x.dtype)
 
 class SwiGLU(nn.Module):
     def __init__(
@@ -90,7 +91,11 @@ class SwiGLU(nn.Module):
     ):
         super().__init__()
         self.d_model = d_model
-        self.d_ff = d_ff
+        if d_ff is None:
+            self.d_ff = int(8 / 3 * d_model)
+            self.d_ff = (self.d_ff + 63) // 64 * 64
+        else:
+            self.d_ff = d_ff
         self.w1 = Linear(d_model, self.d_ff, device=device, dtype=dtype)
         self.w2 = Linear(self.d_ff, self.d_model, device=device, dtype=dtype)
         self.w3 = Linear(self.d_model, self.d_ff, device=device, dtype=dtype)
@@ -188,12 +193,12 @@ def scaled_dot_product_attention(
     to 1, and the attention probabilities of positions with a mask value of False should be zero.
     """
 
-    d_k = Q.size(-1)
+    d_k = Q.shape[-1]
     # 第 i 个 query 对第 j 个 key 的相关性（相似度）
     scores = einsum(Q, K, "... n d_k, ... m d_k -> ... n m") / math.sqrt(d_k)
 
     if mask is not None:
-        scores = scores.masked_fill(mask == False, float('-inf'))  # 屏蔽位置设为 -inf
+        scores = scores.masked_fill(mask == 0, float('-inf'))  # 屏蔽位置设为 -inf
     
     attn = softmax(scores, dim=-1) # 某个query对所有key的相关性 进行softmax归一化
 
@@ -217,6 +222,8 @@ class MHSA(nn.Module):
         self.head_dim = d_model // num_heads
         self.theta = theta
         self.max_seq_len = max_seq_len
+        self.device = device
+        self.dtype = dtype
         # Linear projections
         self.qkv_proj = Linear(d_model, 3 * d_model, device=device, dtype=dtype)
         self.out_proj = Linear(d_model, d_model, device=device, dtype=dtype)
@@ -226,7 +233,6 @@ class MHSA(nn.Module):
     def forward(
             self, 
             x: Float[Tensor, " ... sequence_length d_in"],
-            mask: Bool[Tensor, " ... sequence_length sequence_length"] | None = None,
             token_positions: Int[Tensor, " ... sequence_length"] | None = None,
     ) -> Float[Tensor, " ... sequence_length d_out"]:
         
@@ -238,6 +244,10 @@ class MHSA(nn.Module):
         if self.theta is not None and token_positions is not None:
             q = self.rope(q, token_positions)
             k = self.rope(k, token_positions)
+
+        seq_len = x.shape[-2]
+        #mask = ~torch.triu(torch.ones(seq_len, seq_len, device=x.device, dtype=torch.bool), diagonal=1).bool()
+        mask = torch.tril(torch.ones((seq_len, seq_len), dtype=torch.bool)).to(self.device)
 
         # Apply scaled dot-product attention
         attn_output = scaled_dot_product_attention(q, k, v, mask)
@@ -253,7 +263,7 @@ class transformer_block(nn.Module):
         d_model: int, # Dimensionality of the transformer block input.
         num_heads: int, # Number of heads to use in multi-headed attention.
         d_ff: int, # Dimensionality of the position-wise feed-forward inner layer.
-        theta: float | None = None, # RoPE parameter.
+        theta: float | None = 10000.0, # RoPE parameter.
         max_seq_len: int | None = None, # RoPE parameter used to pre-cache.
         device: torch.device | None = None,
         dtype: torch.dtype | None = None,
@@ -265,18 +275,17 @@ class transformer_block(nn.Module):
         self.device = device
         self.dtype = dtype
         
-        self.attn = MHSA(d_model, num_heads, theta, max_seq_len)
-        self.ffn = SwiGLU(d_model, d_ff)
-        self.ln1 = RMSNorm(d_model)
-        self.ln2 = RMSNorm(d_model)
+        self.attn = MHSA(d_model, num_heads, theta, max_seq_len, device=device)
+        self.ffn = SwiGLU(d_model, d_ff, device=device)
+        self.ln1 = RMSNorm(d_model, device=device)
+        self.ln2 = RMSNorm(d_model, device=device)
 
     def forward(
         self,
         x: Float[Tensor, " ... sequence_length d_model"],
-        mask: Bool[Tensor, "... sequence_length sequence_length"] | None = None, # mask attention
-        token_positions: Int[Tensor, "... sequence_length"] | None = None,# RoPE
     ) -> Float[Tensor, "... sequence_length d_model"]:
-        x = x + self.attn(self.ln1(x), mask, token_positions) # first sub layer for self attention
+        token_positions = torch.arange(x.shape[-2], dtype=torch.int, device=x.device)  # (batch, ..., seq_len)
+        x = x + self.attn(self.ln1(x), token_positions) # first sub layer for self attention
         x = x + self.ffn(self.ln2(x)) # second sub layer for position-wise feed forward
         return x
 
@@ -321,25 +330,24 @@ class transformer_lm(nn.Module):
         self.device = device
         self.dtype = dtype
         
-        self.token_embeddings = Embedding(vocab_size, d_model)
+        self.token_embeddings = Embedding(vocab_size, d_model, device=device)
         self.layers = nn.ModuleList([
-            transformer_block(d_model, num_heads, d_ff, rope_theta, context_length) 
+            transformer_block(d_model, num_heads, d_ff, rope_theta, context_length, device=device) 
             for _ in range(num_layers)
         ])
-        self.ln_final = RMSNorm(d_model)
-        self.lm_head = Linear(d_model, vocab_size)
+        self.ln_final = RMSNorm(d_model, device=device)
+        self.lm_head = Linear(d_model, vocab_size, device=device)
 
 
     def forward(
             self,
             x: Int[Tensor, " batch_size sequence_length"],
-            mask: Bool[Tensor, " batch_size sequence_length sequence_length"] | None = None,
             token_positions: Int[Tensor, " batch_size sequence_length"] | None = None,
     ) -> Float[Tensor, " batch_size sequence_length vocab_size"]:
 
         x = self.token_embeddings(x) # (batch_size, sequence_length, d_model)
         for layer in self.layers:
-            x = layer(x, mask, token_positions) # (batch_size, sequence_length, d_model)
+            x = layer(x) # (batch_size, sequence_length, d_model)
         x = self.ln_final(x) # (batch_size, sequence_length, d_model)
         x = self.lm_head(x) # (batch_size, sequence_length, vocab_size)
         # 不在这里进行softmax, 因为直接在最后利用log-softmax计算loss

scripts/train_ts.py
@@ -38,7 +38,7 @@ def train():
         "batch_size": 16,         # 批次大小
         "total_epochs": 0.5,      # 训练轮数
         "checkpoint_freq": 2000,  # 每隔多少步保存一次检查点
-        "log_freq": 10,           # 每隔多少步记录一次日志
+        "log_freq": 1,           # 每隔多少步记录一次日志
         "val_freq": 400,          # 每隔多少步在验证集上评估
         "val_batch_size": 16,     # 验证时的批次大小
         "val_batches": 20,        # 验证时使用的批次数量
@@ -132,7 +132,7 @@ def train():
             iteration=step,
             max_learning_rate=optim_config["lr"],
             min_learning_rate=optim_config["lr"] * 0.01,
-            warmup_iters=min(500,int(0.1 * total_steps)),
+            warmup_iters=int(0.05 * total_steps),
             anneal_iters=total_steps,
         )
         for param_group in optimizer.param_groups:
@@ -154,15 +154,6 @@ def train():
 
         # 反向传播和优化参数
         loss.backward()
-
-        # 可视化每一层的梯度范数
-        if step % train_config["log_freq"] == 0:
-            grad_stats = {}
-            for name, param in model.named_parameters():
-                if param.grad is not None:
-                    grad_norm = param.grad.data.norm(2).item()
-                    grad_stats[f"grad_l2_norm/{name}"] = grad_norm
-            wandb.log(grad_stats | {"step": step})  # 用 | 合并两个字典
         
         # 计算梯度的 L2 范数
         if step % train_config["log_freq"] == 0:
@@ -224,68 +215,22 @@ def train():
     # 关闭wandb
     wandb.finish()
 
-@torch.no_grad()
-def generate(
-    model: torch.nn.Module,
-    prompt: str,
-    tokenizer,
-    max_new_tokens: int = 100,
-    temperature: float = 1.0,
-    top_p: float = 0.9,
-    end_token_id: int = None,
-    device: torch.device | None = None,
-) -> str:
-    model.eval()
-    if device is None:
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
-    # encode the prompt
-    input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
-    generated = rearrange(input_ids.clone(), "seq_len -> 1 seq_len")
-    # generated: (batch_size=1, seq_len)
-
-    for _ in range(max_new_tokens):
-        context = generated[:, -model.context_length:]
-        logits = model(context)  # 假设输出 shape 为 (1, seq_len, vocab_size)
-        logits = logits[:, -1, :]  # 取最后一个位置的 logits
-
-        logits = logits / temperature
-        probs = MyModules.softmax(logits, dim=-1)
-
-        # Top-p sampling
-        sorted_probs, sorted_indices = torch.sort(probs, descending=True)
-        cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
-
-        sorted_mask = cumulative_probs > top_p        # True 的位置表示要屏蔽
-        sorted_mask[..., 1:] = sorted_mask[..., :-1]  # 向右移一位：保留第一个超过 top_p 的 token
-        sorted_mask[..., 0] = False                   # 保底，防止第一个 token 被屏蔽
-
-
-        sorted_probs[sorted_mask] = 0.0 # boolean indexing
-        sorted_probs = sorted_probs / sorted_probs.sum(dim=-1, keepdim=True)
-
-        next_token_index = torch.multinomial(sorted_probs, num_samples=1)
-        next_token = sorted_indices.gather(dim=-1, index=next_token_index)
-
-        generated = torch.cat((generated, next_token), dim=-1)
-
-        if end_token_id is not None and next_token.item() == end_token_id:
-            break
-
-    return tokenizer.decode(generated[0].tolist()) # generated[0]: Tensor(seq_len) -to_list-> [seq_len]
 
 if __name__ == "__main__":
-    log_path = WORK_DIR / "logs/train_v0.log"
+    log_path = WORK_DIR / "logs/train_v1.log"
     logger.add(log_path, rotation="1 day", retention="7 days", level="INFO")
     TRAIN_DATA_PATH = DATA_DIR / "tinystories_train_ids.npy"
-    VAL_DATA_PATH = None  # 验证集路径
+    VAL_DATA_PATH = DATA_DIR / "tinystories_valid_ids.npy"  # 验证集路径
     CHECKPOINT_LOAD_PATH = None  # 模型检查点路径
-    CHECKPOINT_SAVE_FORMAT = MODEL_DIR / "checkpoints/checkpoint_v0_{}.pt"  # 检查点保存路径格式
-    FINAL_MODEL_PATH = MODEL_DIR / "finals/final_model_v0.pt"  # 最终模型保存路径
+    CHECKPOINT_SAVE_FORMAT = MODEL_DIR / "checkpoints/checkpoint_v1_{}.pt"  # 检查点保存路径格式
+    FINAL_MODEL_PATH = MODEL_DIR / "finals/final_model_v1.pt"  # 最终模型保存路径
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--disable-wandb", action="store_true", help="Disable Weights & Biases logging")
+    parser.add_argument("--offline-wandb", action="store_true", help="Disable Weights & Biases logging")
     args = parser.parse_args()
     if args.disable_wandb:
         os.environ["WANDB_MODE"] = "disabled"
+    if args.offline_wandb:
+        os.environ["WANDB_MODE"] = "offline"
     train()

tests/adapters.py
@@ -149,9 +149,7 @@ def run_multihead_self_attention(
     mhsa = MyModules.MHSA(d_model, num_heads)
     qkv_proj_weight = torch.cat([q_proj_weight, k_proj_weight, v_proj_weight], dim=0)
     mhsa.load_state_dict({"qkv_proj.weight": qkv_proj_weight, "out_proj.weight": o_proj_weight})
-    seq_len = in_features.shape[-2]
-    causal_mask = ~torch.triu(torch.ones(seq_len, seq_len, device=in_features.device, dtype=torch.bool), diagonal=1).bool()
-    return mhsa(in_features, mask=causal_mask)
+    return mhsa(in_features)
     raise NotImplementedError
 
 
@@ -195,9 +193,7 @@ def run_multihead_self_attention_with_rope(
     mhsa = MyModules.MHSA(d_model, num_heads, theta, max_seq_len)
     qkv_proj_weight = torch.cat([q_proj_weight, k_proj_weight, v_proj_weight], dim=0)
     mhsa.load_state_dict({"qkv_proj.weight": qkv_proj_weight, "out_proj.weight": o_proj_weight})
-    seq_len = in_features.shape[-2]
-    causal_mask = ~torch.triu(torch.ones(seq_len, seq_len, device=in_features.device, dtype=torch.bool), diagonal=1).bool()
-    return mhsa(in_features, token_positions=token_positions, mask=causal_mask)
+    return mhsa(in_features, token_positions=token_positions)
     raise NotImplementedError
 
 
@@ -297,10 +293,7 @@ def run_transformer_block(
     """
     tf = MyModules.transformer_block(d_model, num_heads, d_ff, theta, max_seq_len)
     tf.load_weights(weights)
-    token_positions = repeat(torch.arange(in_features.shape[-2], device=in_features.device), 's -> b s', b=in_features.shape[0])
-    seq_len = in_features.shape[-2]
-    causal_mask = ~torch.triu(torch.ones(seq_len, seq_len, device=in_features.device, dtype=torch.bool), diagonal=1).bool()
-    return tf(in_features,mask=causal_mask, token_positions=token_positions)
+    return tf(in_features)
 
     raise NotImplementedError
 
@@ -386,10 +379,7 @@ def run_transformer_lm(
     """
     ts_lm = MyModules.transformer_lm(vocab_size, context_length, num_layers, d_model, num_heads, d_ff, rope_theta)
     ts_lm.load_weights(weights)
-    token_positions = repeat(torch.arange(in_indices.shape[-1], device=in_indices.device), 's -> b s', b=in_indices.shape[0])
-    seq_len = in_indices.shape[-1]
-    causal_mask = ~torch.triu(torch.ones(seq_len, seq_len, device=in_indices.device, dtype=torch.bool), diagonal=1).bool()
-    return ts_lm(in_indices, mask=causal_mask, token_positions=token_positions)
+    return ts_lm(in_indices)
     raise NotImplementedError
 
 